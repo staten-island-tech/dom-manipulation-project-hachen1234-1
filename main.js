e9cfff2eccffe608107d723a95b9243af09d8c83
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-10-16 10:17:14 -0400

    1

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const DOMselector = {
   form: document.querySelector("#form"),
-  textbox: document.querySelector(".first-name"),
+  names: document.querySelector(".names"),
+  age: document.querySelector(".age"),
+  dumbquotes: document.querySelector(".dumb-quotes"),
   h2s: document.querySelectorAll("h2"),
-  //select the text box
-  //select all the h2
+  button: document.querySelector("#button"),
 };
-DOMselector.form.addEventListener("submit", function (event) {
+DOMselector.button.addEventListener("click", function (event) {
   event.preventDefault;
-  console.log(DOMselector.textbox.value);
+  DOMselector.form.insertAdjacentHTML(
+    "afterend",
+    `
+  <div class="display-card">
+  <h1 class="display-name" src="${DOMselector.names.value}/>
+  <h2 class="display-age">${DOMselector.age.value}</h2>
+  <h3 class="display-dumbquotes">${DOMselector.dumbquotes.value}</h3>
+  <button onclick = clear()" class="remove btn">Remove Album</button>
+</div>
+`,
+    function clear() {
+      DOMselector.names.value = "";
+      DOMselector.age.value = "";
+      DOMselector.dumbquotes.value = "";
+    }
+  );
 });
