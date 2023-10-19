b59e0814cb5a4dad5876bf405233bc78eb374042
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-10-19 09:31:21 -0400

    1

--- main.js
+++ main.js
@@ -8,20 +8,24 @@ const DOMselector = {
 };
 DOMselector.button.addEventListener("click", function (event) {
   event.preventDefault;
-  DOMselector.form.insertAdjacentHTML(
-    "afterend",
-    `
+  function insertcard() {
+    DOMselector.form.insertAdjacentHTML(
+      "afterend",
+      `
   <div class="display-card">
   <h1 class="display-name" src="${DOMselector.names.value}/>
   <h2 class="display-age">${DOMselector.age.value}</h2>
   <h3 class="display-dumbquotes">${DOMselector.dumbquotes.value}</h3>
-  <button onclick = clear()" class="remove btn">Remove Album</button>
-</div>
-`,
+  <button onclick"clear" class="remove-btn">Remove Album</button>
+  </div>
+  `
+    );
     function clear() {
       DOMselector.names.value = "";
       DOMselector.age.value = "";
       DOMselector.dumbquotes.value = "";
     }
-  );
+    clear();
+  }
+  insertcard();
 });
